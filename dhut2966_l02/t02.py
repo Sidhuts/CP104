b747eea9f80438cbb58d0f73db132a7a03da9757
Sidhuts <162533876+Sidhuts@users.noreply.github.com>
2025-01-29 22:44:05 -0500

    Add files via upload

---
+++ dhut2966_l02/t02.py
@@ -0,0 +1,16 @@
+"""
+------------------------------------------------------------------------
+[program description]
+------------------------------------------------------------------------
+Author: Sidharth Dhutti
+ID:      169062966
+Email:  dhut2966@mylaurier.ca
+__updated__ = "2023-09-20"
+------------------------------------------------------------------------
+"""
+Fahrenheit = int(input("Temperature (F): "))
+
+WATER = 32
+Celsius = (Fahrenheit - 32) * (5/9)
+print()
+print("Temperature (C):",Celsius)
