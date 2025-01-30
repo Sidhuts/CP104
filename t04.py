dc498a30233422d305cd492f544724cefcb38038
Sidhuts <162533876+Sidhuts@users.noreply.github.com>
2025-01-29 22:35:29 -0500

    Add files via upload

---
+++ t04.py
@@ -0,0 +1,16 @@
+"""
+------------------------------------------------------------------------
+[program description]
+------------------------------------------------------------------------
+Author: Sidharth Dhutti
+ID:      169062966
+Email:  dhut2966@mylaurier.ca
+__updated__ = "2023-09-20"
+------------------------------------------------------------------------
+"""
+n1 = int(input("First numerator: "))
+d1 = int(input("First denominator: "))
+n2 = int(input("Second numerator: "))
+d2 = int(input("Second denominator: "))
+r = (n1 * n2) / (d1 * d2)
+print("Product: ",r)
