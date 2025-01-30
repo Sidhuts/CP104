a08b0ddd7797f43f6259206ab2afed5675c0a8b9
Sidhuts <162533876+Sidhuts@users.noreply.github.com>
2025-01-29 22:42:44 -0500

    Add files via upload

---
+++ dhut2966_l02/t01.py
@@ -0,0 +1,16 @@
+"""
+------------------------------------------------------------------------
+[program description]
+------------------------------------------------------------------------
+Author: Sidharth Dhutti
+ID:      169062966
+Email:  dhut2966@mylaurier.ca
+__updated__ = "2023-09-20"
+------------------------------------------------------------------------
+"""
+#imports
+WATERFREEZE = 32
+Celsius = int(input("Temperature (C): "))
+print()
+fahrenheit = (9/5) * Celsius + WATERFREEZE
+print("Temperature (F): ", fahrenheit)
